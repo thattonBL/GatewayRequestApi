fa5e9a1f6dedd23d130352b6cc3e775cbcdbd921
thatton <thomasrichardhatton@googlemail.com>
2024-06-11 18:00:42 +0100

    Make swagger available in production

--- GatewayRequestApi/Program.cs
+++ GatewayRequestApi/Program.cs
@@ -62,11 +62,11 @@ namespace GatewayRequestApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
             app.UseCors("AllowAll");
             app.UseHttpsRedirection();
             app.UseAuthorization();

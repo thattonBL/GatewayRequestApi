a0a8fe09c9299ae686a7df97b93da6f7aaa06abe
thatton <thomasrichardhatton@googlemail.com>
2024-11-20 13:39:58 +0000

    Temporary Get on the environment vars for Azure Debugging
    - Bringing environment vars inline with Azure
    - Remove elastic and Kibana from Docker Compose
    - Add temp controller for viewing Env Vars

---
+++ GatewayRequestApi/Controllers/EnvironmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GatewayRequestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvironmentController : ControllerBase
+    {
+        // GET: api/<EnvironmentController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var sqlConn = Environment.GetEnvironmentVariable("SQL_DB_CONNECTION_STRING");
+            var appInsConn = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
+            var servBusConn = Environment.GetEnvironmentVariable("AZURE_SERVICE_BUS_CONNECTION_STRING");
+            return new string[] { sqlConn, appInsConn, servBusConn };
+        }
+    }
+}
